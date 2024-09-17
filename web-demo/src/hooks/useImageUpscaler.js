034e75eee17f2b0c049a4853ed6a081e32e70856
minhanh01bg <vu.anh147852@gmail.com>
2024-09-17 11:58:22 +0700

    update

--- web-demo/src/hooks/useImageUpscaler.js
+++ web-demo/src/hooks/useImageUpscaler.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
-import { postImages } from '../services/imageUpscalerService';
+import { postImages, getTaskStatus } from '../services/imageUpscalerService';
+
 const useImageUpscaler = (showErrorNotification, showSuccessNotification) => {
   const [formData, setFormData] = useState({ file: null, type: 'front' })
   const [file, setFile] = useState("");
   const [upscaled, setUpscaled] = useState("");
-  
+  const [taskId, setTaskId] = useState("");
+  const [status, setStatus] = useState("")
+
   const handleFileChange = (event) => {
     setFormData({ ...formData, file: event.target.files[0] });
   };
-
- 
 
   const clearForm = () => {
     setFormData({
@@ -20,15 +21,34 @@ const useImageUpscaler = (showErrorNotification, showSuccessNotification) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // Handle form submission
-    // showErrorNotification('process is running');
     const res = await postImages(formData, showErrorNotification);
     if (res !== undefined) {
+      setTaskId(res.task_id);
       
-      setFile(res.image);
-      setUpscaled(res.upscaled)
+      // Bắt đầu kiểm tra trạng thái với setInterval
+      const interval = setInterval(async () => {
+        const taskStatus = await checkTaskStatus(res.task_id);
+        
+        // Dừng interval nếu task hoàn thành hoặc gặp lỗi
+        if (taskStatus === "SUCCESS" || taskStatus === "FAILURE") {
+          clearInterval(interval);  // Dừng việc kiểm tra
+        }
+      }, 5000); // Kiểm tra mỗi 5 giây
     }
   };
+  
+  const checkTaskStatus = async (taskId) => {
+    const res = await getTaskStatus(taskId);
+    setStatus(res.status);
+    
+    if (res.status === "SUCCESS") {
+      setFile(res.result.image);
+      setUpscaled(res.result.upscaled);
+    }
+    
+    return res.status; // Trả về trạng thái để dừng interval nếu cần
+  };
+  
 
   return {
     file,
@@ -37,7 +57,8 @@ const useImageUpscaler = (showErrorNotification, showSuccessNotification) => {
     handleFileChange,
     handleSubmit,
     clearForm,
-    setFormData
+    setFormData,
+    status
   };
 }
 
