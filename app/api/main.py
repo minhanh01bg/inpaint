afdc8f9e8dc1bd86bc072dd5a99d206b18d0482c
minhanh01bg <vu.anh147852@gmail.com>
2024-09-23 09:59:43 +0700

    update

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import inpaint_controller, rembg_controller, user_controller, upscaler_route
+from app.api.routes import inpaint_controller, rembg_controller, upscaler_controller, user_controller
 
 api_router = APIRouter()
 api_router.include_router(rembg_controller.router,prefix='',tags=['test'])
 api_router.include_router(user_controller.router,prefix='',tags=['user'])
 api_router.include_router(inpaint_controller.router,prefix='',tags=['inpaint'])
-api_router.include_router(upscaler_route.router, prefix='', tags=['upscaler'])
+api_router.include_router(upscaler_controller.router, prefix='', tags=['upscaler'])
