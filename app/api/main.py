c5f167d07b717b75c838559a54fca5abad0fedbf
minhanh01bg <vu.anh147852@gmail.com>
2024-09-26 19:07:51 +0700

    update

--- app/api/main.py
+++ app/api/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.api.routes import inpaint_controller, rembg_controller, upscaler_controller, user_controller
 # from app.api.routes import inpaint_controller, rembg_controller, user_controller
 api_router = APIRouter()
-api_router.include_router(rembg_controller.router,prefix='',tags=['test'])
+api_router.include_router(rembg_controller.router,prefix='',tags=['Background removal'])
 api_router.include_router(user_controller.router,prefix='',tags=['user'])
 api_router.include_router(inpaint_controller.router,prefix='',tags=['inpaint'])
 api_router.include_router(upscaler_controller.router, prefix='', tags=['upscaler'])
